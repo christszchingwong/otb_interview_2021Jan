350a806cc57dcf78829224eb912293f0faed6e5b
Chris TC Wong <christszchingwong@gmail.com>
2021-01-18 15:45:20 +0000

    yo typescript-jest

---
+++ src/e-otb-interview-2021-jan.test.ts
@@ -0,0 +1,6 @@
+import { EOtbInterview2021Jan } from './e-otb-interview-2021-jan';
+
+test('EOtbInterview2021Jan is instantiated', () => {
+  const eOtbInterview2021Jan = new EOtbInterview2021Jan();
+  expect(eOtbInterview2021Jan).toBeInstanceOf(EOtbInterview2021Jan);
+});
---
+++ src/e-otb-interview-2021-jan.ts
@@ -0,0 +1,1 @@
+export class EOtbInterview2021Jan {}
