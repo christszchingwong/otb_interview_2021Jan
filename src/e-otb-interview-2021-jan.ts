012630de4d80a09ae8864e2fc608f90d8e248105
Chris TC Wong <christszchingwong@gmail.com>
2021-01-18 17:55:20 +0000

    simple input parser

--- src/e-otb-interview-2021-jan.ts
+++ src/e-otb-interview-2021-jan.ts
@@ -1,4 +1,21 @@
+const schedulerInputParser: (input: string) => Array<{task: string, dependencies: string[]}> = (input) => {
+    const nonEmptyLines = input.split(`\r\n`).map(line => line.trim()).filter(line => line != ``);
+    return nonEmptyLines.map(line => {
+        const matches = /(.*)=>(.*)/.exec(line);
+        const task = matches[1].trim();
+        const dependencies = matches[2].trim().split(``);
+        return {
+            task,
+            dependencies
+        };
+    });
+};
+
 const scheduler: (input: string) => string[] = (input) => {
+    // simple case first
+    if (input.length == 0) return [];
+
+    let dependency = schedulerInputParser(input);
     let result: string[] = [];
     // TODO Implement the method
     return result;
