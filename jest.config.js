49ccaaa303ba2fbf22d8cf107ec27177aeee9e0c
Chris TC Wong <christszchingwong@gmail.com>
2021-01-18 18:41:38 +0000

    build ts

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)$",
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json"
+  ],
 }
+
